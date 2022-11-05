1f287406255fd7266a726f59739993f9547881bb
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2022-11-05 22:34:47 +0100

    Commit du 05/11/2022
    Ajout d'une fonctionnalité de zoom sur les photographies des jeux. JS/HTML/CSS
    
    Vérification : OK
    Responsive : OK

--- js/main.js
+++ js/main.js
@@ -52,3 +52,16 @@ function mufunction(){
     var scrolled = (winScroll / height) * 100;
     document.getElementById('scroll-bar').style.width = scrolled + '%';
 }
+
+
+//images
+
+document.querySelectorAll('.screenshots-content img').forEach(image =>{
+  image.onclick = () =>{
+      document.querySelector('.popup-image').style.display = 'block';
+      document.querySelector('.popup-image img').src = image.getAttribute('src');
+  }
+});
+document.querySelector('.popup-image span').onclick = () =>{
+  document.querySelector('.popup-image').style.display = 'none';
+}
